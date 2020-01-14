814f8214ee1a8df290ec4109195b76226090c1f4
Manami Ueda <manamiueda1@gmail.com>
2020-01-14 11:41:05 -0500

    changed visa to credit card

--- server/db/models/payment.js
+++ server/db/models/payment.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 const Payment = db.define('payments', {
   type: {
-    type: Sequelize.ENUM('visa', 'paypal', 'stripe')
+    type: Sequelize.ENUM('credit card', 'paypal', 'stripe')
   }
 })
 module.exports = Payment
