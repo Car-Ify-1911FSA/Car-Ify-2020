700acfcd59885533b5052cc1a038c0bf39a63d47
Slava Adnavorau <slava.adnavorau@gmail.com>
2020-01-14 15:43:48 -0500

    product routes added

---
+++ server/api/products.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.send(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
