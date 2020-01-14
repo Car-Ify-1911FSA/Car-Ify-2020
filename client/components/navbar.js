66d6576e6ed0f735afc68b64149c32a9fb512d34
James Shen <jjss886@gmail.com>
2020-01-13 19:40:49 -0500

    Updated Navbar layout and added Checkout placeholder

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,25 +5,32 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
+  <div className="navBarDiv">
+    <div className="navBarInsideDiv">
+      <h1>Welcome to Car-ify !</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home" className="navBarText">
+              Home
+            </Link>
+            <Link to="/checkout" className="navBarText">
+              ! BUILD Checkout BUILD !
+            </Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+    </div>
     <hr />
   </div>
 )
