18c7f45f58d7450da9d20e280070c3bee56dd9e0
Slava Adnavorau <slava.adnavorau@gmail.com>
2020-01-15 12:29:23 -0500

    just a get route for all orders for specific user

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
 const {Order} = require('../db/models')
 
-router.post('/', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
-    const newOrder = await Order.create(req.body)
-    res.send(newOrder)
+    const allOrders = await Order.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    })
+    res.send(allOrders)
   } catch (error) {
     console.log(error)
   }
