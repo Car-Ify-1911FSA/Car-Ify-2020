b1f7d4de0784cf82afdc3981f7098edda003942e
James Shen <jjss886@gmail.com>
2020-01-15 18:59:25 -0500

    Updated Payment Account name

--- server/db/models/PaymentAccount.js
+++ server/db/models/PaymentAccount.js
@@ -1,13 +1,10 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const PaymentAccounts = db.define('payment_accounts', {
-  // type: {
-  //   type: Sequelize.STRING
-  // },
   name: {
     type: Sequelize.STRING
   }
-})
+});
 
-module.exports = PaymentAccounts
+module.exports = PaymentAccounts;
