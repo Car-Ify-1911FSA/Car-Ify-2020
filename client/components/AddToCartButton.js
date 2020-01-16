ffa6d51e1f42c2edd0bb3c1a1964ce9af18003b5
James Shen <jjss886@gmail.com>
2020-01-16 11:58:07 -0500

    Able to post new item to existing cart

--- client/components/AddToCartButton.js
+++ client/components/AddToCartButton.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {getActiveCart, adddNewCart, addNewCartDetail} from '../store';
+import {
+  getActiveCart,
+  getCartDetail,
+  adddNewCart,
+  addNewCartDetail
+} from '../store';
 
 class AddToCartButton extends Component {
   constructor() {
@@ -9,20 +14,31 @@ class AddToCartButton extends Component {
     this.handleAddClick = this.handleAddClick.bind(this);
   }
 
-  componentDidMount() {
-    this.props.getCart(this.props.userId);
+  async componentDidMount() {
+    await this.props.getCart(this.props.userId);
+    await this.props.getCartDetail(this.props.cartId);
   }
 
-  handleAddClick(productId) {
-    const {isLoggedIn, userId, cartId} = this.props;
-
-    // FOLLOWING LOGIC:
-    // IF CARTID AVAILABLE (SIGNIFYING "ACTIVE" ONE), POST THERE WITH PRODID
-    // OTHERWISE CREATE NEW CARTID WITH USERID AND POST TO BOTH TABLES
+  handleAddClick(productId, productPrice) {
+    const {isLoggedIn, userId, cartId, cart, cartDetail} = this.props;
 
     if (isLoggedIn) {
-      if (cartId) console.log('POST TO CARTPROD');
-      else console.log('POST TO CART AND CARTPROD');
+      if (cartId) {
+        let prodIdArr = cartDetail.map(prod => prod.productId);
+        if (prodIdArr.includes(productId)) {
+          console.log('PUT || ONLY TO CARTPROD', productId, cart, cartDetail);
+        } else {
+          const addCartItemObj = {
+            cartId: cartId,
+            productId: productId,
+            quantity: 1,
+            totalPrice: productPrice
+          };
+          this.props.addCartItem(isLoggedIn, addCartItemObj);
+        }
+      } else {
+        console.log('POST TO CART AND CARTPROD', cartId, this.props);
+      }
     } else {
       console.log('Need Local Storage Functionality for Guests');
     }
@@ -33,7 +49,9 @@ class AddToCartButton extends Component {
       <div className="addBtnDiv">
         <button
           type="button"
-          onClick={() => this.handleAddClick(this.props.productId)}
+          onClick={() =>
+            this.handleAddClick(this.props.product.id, this.props.product.price)
+          }
           className="addBtn"
         >
           Add to Cart
@@ -47,15 +65,18 @@ const mapState = state => {
   return {
     isLoggedIn: !!state.user.id,
     userId: state.user.id,
-    cartId: state.cart.id
+    cartId: state.cart.id,
+    cart: state.cart,
+    cartDetail: state.cartProduct
   };
 };
 
 const mapDispatch = dispatch => {
   return {
-    getCart: userId => {
-      dispatch(getActiveCart(userId));
-    }
+    getCart: userId => dispatch(getActiveCart(userId)),
+    getCartDetail: cartId => dispatch(getCartDetail(cartId)),
+    addCartItem: (isLoggedIn, newCartItem) =>
+      dispatch(addNewCartDetail(isLoggedIn, newCartItem))
   };
 };
 
