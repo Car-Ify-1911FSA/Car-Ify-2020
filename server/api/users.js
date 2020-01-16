b5b604cef07b681f44a4b091c820316597b902fd
Manami Ueda <manamiueda1@gmail.com>
2020-01-15 19:09:28 -0500

    one part of security is finished but need to figure out how the logged in user can access own info

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const {User} = require('../db/models');
+// const {ensureAuthenticated} = require('./index');
 
-router.get('/', async (req, res, next) => {
+function ensureAuthenticated(req, res, next) {
+  if (req.user.admin) next();
+  else res.sendStatus(403);
+}
+
+router.get('/', ensureAuthenticated, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
