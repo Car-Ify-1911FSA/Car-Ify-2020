0dfe090662f6b60002f77ce7662c95cfde50a350
Manami Ueda <manamiueda1@gmail.com>
2020-01-21 18:20:17 -0500

    security is working

--- server/api/security.js
+++ server/api/security.js
@@ -1,3 +1,9 @@
+function isUserOrAdmin(req, res, next) {
+  // console.log('req.session------------>', req.session.passport.user);
+  if (req.user) next();
+  else res.sendStatus(403);
+}
+
 //Admin rights
 function isAdmin(req, res, next) {
   if (req.user.admin) next();
@@ -5,20 +11,15 @@ function isAdmin(req, res, next) {
 }
 
 // is loggedin user
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) next();
-  else res.sendStatus(403);
-}
+// function isLoggedIn(req, res, next) {
+//   console.log('req.isAuthenticated---------------->', req.isAuthenticated());
+//   if (req.isAuthenticated()) next();
+//   else res.sendStatus(403);
+// }
 
 //loggedin users and admin rights
-function isUserOrAdmin(req, res, next) {
-  console.log('req.session------------>', req.session.passport.user);
-  if (req.session.passport.user == req.user.userId || req.user.admin) next();
-  else res.sendStatus(403);
-}
 
 module.exports = {
   isAdmin,
-  isLoggedIn,
   isUserOrAdmin
 };
