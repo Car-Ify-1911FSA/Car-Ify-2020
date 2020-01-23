a72df8a59a8b3978576928ee409616fbbe9c29b8
James Shen <jjss886@gmail.com>
2020-01-23 11:08:28 -0500

    cart total div css

--- client/components/cart/Cart.js
+++ client/components/cart/Cart.js
@@ -89,8 +89,14 @@ class Cart extends Component {
 
         <div className="cartTotalDiv">
           <h3 className="headerDiv">Cart Summary</h3>
-          <h4>Total Quantity: {this.calcTotalQuantity(cartDetail)}</h4>
-          <h4>Total Price: {this.calcTotalPrice(cartDetail)}</h4>
+          <div className="cartTotalItems">
+            <h4 className="cartTotalText">
+              Total Quantity: {this.calcTotalQuantity(cartDetail)}
+            </h4>
+            <h4 className="cartTotalText">
+              Total Price: {this.calcTotalPrice(cartDetail)}
+            </h4>
+          </div>
         </div>
 
         <div className="cartOuterBtnDiv">
