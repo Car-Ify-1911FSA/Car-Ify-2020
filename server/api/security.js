63ce080a15781965b1b2f956fd7048dd76617122
Manami Ueda <manamiueda1@gmail.com>
2020-01-23 11:41:24 -0500

    final edits

--- server/api/security.js
+++ server/api/security.js
@@ -2,15 +2,15 @@
 function isAdmin(req, res, next) {
   if (req.user.admin) next();
   // else res.sendStatus(403);
-  else res.status(403);
+  else res.sendStatus(403);
 }
+
+//loggedin users and admin rights
 
 function isUserOrAdmin(req, res, next) {
   if (req.user) next();
   else res.sendStatus(403);
 }
-
-//loggedin users and admin rights
 
 module.exports = {
   isAdmin,
