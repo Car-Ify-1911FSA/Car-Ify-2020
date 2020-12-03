0b2711163681f32255156bf11ad18d79c7970bd6
JP <polanco.joseph1@gmail.com>
2020-12-03 00:14:34 -0500

    commit

--- server/db/db.js
+++ server/db/db.js
@@ -4,12 +4,8 @@ const pkg = require('../../package.json');
 const databaseName =
   pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
-console.log(databaseName, 'databaseName');
-console.log(process.env, 'databaseUrl');
-
-// ${databaseName}
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/d4c3f7b45klt8b`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: console.log
   }
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,6 +5,8 @@ const Payment = require('./payment');
 const PaymentAccount = require('./PaymentAccount');
 const CartProduct = require('./cartProduct');
 
+console.log('Creating models');
+
 Cart.belongsToMany(Product, {
   through: {model: CartProduct}
 });
@@ -12,11 +14,15 @@ Product.belongsToMany(Cart, {
   through: {model: CartProduct}
 });
 
+console.log('Creating models 2');
+
 Cart.belongsTo(User);
 Cart.belongsTo(PaymentAccount);
 
 PaymentAccount.belongsTo(Payment);
 PaymentAccount.belongsTo(User);
+
+console.log('Creating models 3');
 
 module.exports = {
   User,
